024e6e6ceb073d8231e798e96e9ea35f652e14a4
Nasc1mento <reisadryan16@gmail.com>
2022-07-17 16:48:28 -0300

    all page css

--- todo/edit-task.php
+++ todo/edit-task.php
@@ -22,19 +22,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/style.css">
-    <title>Document</title>
+    <title>Edit Task</title>
 </head>
 <body>
+    
+    <div class="edit-task">
     <h1>TODO LIST</h1>
-    <fieldset>
-        <legend>Edit task</legend> 
-        <form action="./update-task.php" method="POST">
-            <input type="text" name="name" value='<?=$name?>'>
-            <input type="hidden" name="id_task" value=<?=$id_task?>>
-            <button type="submit">Update</button>
-        </form>
-    </fieldset>
-    <a href="./index.php">Back</a>
+        <fieldset>
+            <legend>Edit task</legend> 
+            <form action="./update-task.php" method="POST">
+                <input type="text" name="name" value='<?=$name?>'>
+                <input type="hidden" name="id_task" value=<?=$id_task?>>
+                <button type="submit"><img src="../icons/upgrade.svg"></button>
+            </form>
+        </fieldset>
+    </div>
+    <a href="./index.php"><button><img src="../icons/back.png"></button></a>
 </body>
 </html>
 
--- todo/index.php
+++ todo/index.php
@@ -20,26 +20,31 @@
     <link rel="stylesheet" href="../style/style.css">
 </head>
 <body>
-    <h1>TODO LIST</h1>
-    
-    <fieldset>
-        <legend>Add task</legend>
-            <form action="add-task.php" method="POST">
-                <input type="text" name="name" placeholder="Name">
-                <button type="submit">Add</button>
-            </form>
-    </fieldset>
-    <table>
-        <?php foreach ($data as $row) :?>
-            <tr>
-                <?php if ($_SESSION['usr_id'] == $row['ITS_USR_ID']):?>
-                    <td><?= $row['ITS_NAME']?></td>
-                    <td><button type="button"><a href="./edit-task.php?id_task=<?=$row['ITS_ID']?>">Edit</a></button></td>
-                    <td><button type="button"><a href="./delete-task.php?id_task=<?=$row['ITS_ID']?>">Delete</a></button></td>
-                <?php endif ?>
-            </tr>   
-        <?php endforeach ?>
+    <div class="add-task">
+        <h1>TODO LIST</h1>
+        <fieldset>
+            <legend>Add task</legend>
+                <form action="add-task.php" method="POST">
+                    <input type="text" name="name" placeholder="Name">
+                    <button type="submit"><img src="../icons/add.svg"></button>
+                </form>
+        </fieldset>
+    </div>
+
+        <table>
+            <?php foreach ($data as $row) :?>
+                <tr>
+                    <?php if ($_SESSION['usr_id'] == $row['ITS_USR_ID']):?>
+                        <td><?= $row['ITS_NAME']?></td>
+                        <td><a href="./edit-task.php?id_task=<?=$row['ITS_ID']?>"><button><img src="../icons/edit.svg" alt="edit"></button></a></td>
+                        <td><a href="./delete-task.php?id_task=<?=$row['ITS_ID']?>" class="delete-button"><button><img src="../icons/delete.svg" alt="delete"></button></a></td>
+                    <?php endif ?>
+                </tr>   
+            <?php endforeach ?>
+            <div class="logout-button">
+                <a href="../user/logout.php"><button><img src="../icons/logout.svg" alt="logout"></button></a>
+            </div>
     </table>
-    <a href="../user/logout.php">Log out</a>
+    
 </body>
 </html>
