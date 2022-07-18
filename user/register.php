084f875d3b6129163082378f518b636412e250b8
Nasc1mento <reisadryan16@gmail.com>
2022-07-18 12:39:11 -0300

    register.php

--- user/register.php
+++ user/register.php
@@ -1,4 +1,4 @@
-<?php include './database/config.php' ?>
+<?php include '../database/config.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,17 +12,18 @@
     <div class="register">
         <h2>Register</h2>
         <form action="./add-user.php" method="POST">
-            <input type="text" name="name" placeholder="Name" value=<?=$_SESSION['name']?>>
-            <input type="email" name="email" placeholder="Email" value=<?=$_SESSION['email']?>>
-            <input type="password" name="password" placeholder="Password">
+            <input type="text" name="name" placeholder="Name" value="<?=$_SESSION['name']?>">
+            <input type="email" name="email" placeholder="Email" value="<?=$_SESSION['email']?>">
+            <input type="password" name="password" placeholder="Password" value="<?=$_SESSION['password']?>">
             <input type="confirm_password" name="confirm_password" placeholder="Confirm password">
             <button type="submit">Create</button>
             <div>
                 <?php if (isset($_GET['err']) && $_GET['err'] == 0): ?>  
-                    <p>The passowords doesn't match</p>
+                    <p>The passwords doesn't match</p>
                 <?php endif ?>
             </div>    
         </form>
     </div>
+    <a href="../index.php"><button><img src="../icons/back.png"></button></a>
 </body>
 </html>
